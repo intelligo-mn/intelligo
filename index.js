1165621c60bbf5745bee2c5aaf39ff04d98b9e26
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-12-23 09:30:44 +0000

    index req update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // index
 app.get('/', function (req, res) {
-	res.send('hello world i am a secret bot')
+	res.send('Сайн уу Pro bot байна уу')
 })
 
 // for facebook verification
