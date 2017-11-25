f22c84475e9147754d5e9e2f8c9f5c0964d9d0dd
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-11-25 15:01:58 +0000

    auth routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,22 +1,69 @@
-const express      = require('express'),
-  router           = express.Router(),
-  mainController   = require('../app/MainController'),
-  dataController   = require('../app/DataController');
+const mainController   = require('../app/MainController'),
+      dataController   = require('../app/DataController');
 
-module.exports = router;
+module.exports = function(app, passport) {
 
-router.get('/', mainController.showHome);
+    app.get('/', mainController.showHome);
+    
+    app.get('/datas',       dataController.showDatas);
+    
+    app.get('/datas/seed',  dataController.seedDatas);
+    
+    app.get('/datas/create',  dataController.showCreate);
+    app.post('/datas/create', dataController.processCreate);
+    
+    app.get('/datas/:slug/edit', dataController.showEdit);
+    app.post('/datas/:slug',     dataController.processEdit);
+    
+    app.get('/datas/:slug/delete', dataController.deleteData);
+    
+    app.get('/datas/:slug', dataController.showSingle);
 
-router.get('/datas',       dataController.showDatas);
+    // =====================================
+    // LOGIN ===============================
+    // =====================================
+    // show the login form
+    app.get('/login', function(req, res) {
 
-router.get('/datas/seed',  dataController.seedDatas);
+        // render the page and pass in any flash data if it exists
+        res.render('login.ejs', { message: req.flash('loginMessage') }); 
+    });
 
-router.get('/datas/create',  dataController.showCreate);
-router.post('/datas/create', dataController.processCreate);
+    // process the login form
+    // app.post('/login', do all our passport stuff here);
 
-router.get('/datas/:slug/edit', dataController.showEdit);
-router.post('/datas/:slug',     dataController.processEdit);
+    // =====================================
+    // SIGNUP ==============================
+    // =====================================
+    // show the signup form
+    app.get('/signup', function(req, res) {
 
-router.get('/datas/:slug/delete', dataController.deleteData);
+        // render the page and pass in any flash data if it exists
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
 
-router.get('/datas/:slug', dataController.showSingle);
+    // process the signup form
+    // app.post('/signup', do all our passport stuff here);
+
+    // =====================================
+    // PROFILE SECTION =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+};
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+    res.redirect('/');
+}
