145ae5210b6987d4f30af9ca26cbe5f0c0d419e3
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-11-25 04:48:49 +0000

    created user schema

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  passwordConf: {
+    type: String,
+    required: true,
+  }
+});
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
