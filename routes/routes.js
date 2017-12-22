b4a5535e989b289da1aaf87518c072fdccf94e39
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-12-22 10:43:04 +0000

    meta tags

--- routes/routes.js
+++ routes/routes.js
@@ -32,7 +32,7 @@ module.exports = function(app, passport) {
     });
     
     app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/', // redirect to the secure profile section
+        successRedirect : '/console', // redirect to the secure profile section
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
