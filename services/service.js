7cdcced4caf426535901283d8edf1a57c11e1ed3
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2018-01-08 01:39:42 +0000

    pinning service some dns rm

--- services/service.js
+++ services/service.js
@@ -2,13 +2,5 @@ module.exports = [
   {
     url: 'http://www.techstar.cloud', // URL of service we'll be pining
     timeout: 200 // threshold in milliseconds above which is considered degraded performance
-  },
-  {
-    url: 'http://www.techstar.cloud/status',
-    timeout: 300
-  },
-  {
-    url: 'https://techstar-bot-tortuvshin.c9users.io',
-    timeout: 300
   }
 ]
