6d57017593084ef4ba5d83b8da2926ee995d24f0
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2018-01-10 17:50:28 +0000

    updated

--- uptime/service.js
+++ uptime/service.js
@@ -2,5 +2,9 @@ module.exports = [
   {
     url: 'https://www.techstar.cloud', // URL of service we'll be pining
     timeout: 200 // threshold in milliseconds above which is considered degraded performance
+  },
+  {
+    url: 'http://techstar-traffic.herokuapp.com', // URL of service we'll be pining
+    timeout: 200 // threshold in milliseconds above which is considered degraded performance
   }
 ]
