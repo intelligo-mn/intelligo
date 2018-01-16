c9bb3d77dcc90ce358ad03736dd9ef449c724c54
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2018-01-16 03:14:04 +0000

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,8 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const extractCSS = new ExtractTextPlugin('[name].fonts.css');
 const extractSCSS = new ExtractTextPlugin('[name].styles.css');
 
-const BUILD_DIR = path.resolve(__dirname, 'build');
-const SRC_DIR = path.resolve(__dirname, 'src');
+const BUILD_DIR = path.resolve(__dirname, 'console/build');
+const SRC_DIR = path.resolve(__dirname, 'console/src');
 
 console.log('BUILD_DIR', BUILD_DIR);
 console.log('SRC_DIR', SRC_DIR);
@@ -55,7 +55,7 @@ module.exports = (env = {}) => {
             use: [
               {
                 loader: 'css-loader',
-                options: {alias: {'../img': '../public/img'}}
+                options: {alias: {'../console/img': '../console/public/img'}}
               },
               {
                 loader: 'sass-loader'
@@ -77,7 +77,7 @@ module.exports = (env = {}) => {
               // loader: 'url-loader'
               loader: 'file-loader',
               options: {
-                name: './img/[name].[hash].[ext]'
+                name: './console/img/[name].[hash].[ext]'
               }
             }
           ]
@@ -86,7 +86,7 @@ module.exports = (env = {}) => {
           test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
           loader: 'file-loader',
           options: {
-            name: './fonts/[name].[hash].[ext]'
+            name: './console/fonts/[name].[hash].[ext]'
           }
         }]
     },
@@ -99,11 +99,11 @@ module.exports = (env = {}) => {
       new HtmlWebpackPlugin(
         {
           inject: true,
-          template: './public/index.html'
+          template: './console/public/index.html'
         }
       ),
       new CopyWebpackPlugin([
-          {from: './public/img', to: 'img'}
+          {from: './console/public/img', to: 'img'}
         ],
         {copyUnmodified: false}
       )
