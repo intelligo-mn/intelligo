f64ee88eb87a406e9027cab41e1c3a82b13b551f
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2018-01-18 06:21:18 +0000

    update

--- uptime/app.js
+++ uptime/app.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const express = require( 'express'),
-      config         = require('config'),
-      websites         = require('./websites'),
-      TechstarBot      = require('techstar-uptime');
+const express  = require( 'express'),
+      config   = require('config'),
+      websites = require('./websites'),
+      Uptime   = require('techstar-uptime');
 
 const app = express();
 
-const bot = new TechstarBot({
+const bot = new Uptime({
   SLACK_WEBHOOK_URL: config.get('SLACK_WEBHOOK_URL')
 });
 bot.monitor(websites);
