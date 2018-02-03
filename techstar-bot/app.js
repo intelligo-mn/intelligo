6d92cfe423ec2343cc7262a0bd13af2cd2aa82ff
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2018-02-03 15:33:27 +0000

    bot update

--- techstar-bot/app.js
+++ techstar-bot/app.js
@@ -6,10 +6,11 @@ const express = require( 'express'),
 
 const app = express();
 
+
 const bot = new TechstarBot({
-  accessToken: config.get('PAGE_ACCESS_TOKEN'),
-  verifyToken: config.get('VALIDATION_TOKEN'),
-  appSecret: config.get('APP_SECRET'),
+  PAGE_ACCESS_TOKEN: (process.env.PAGE_ACCESS_TOKEN) ? (process.env.PAGE_ACCESS_TOKEN) : config.get('PAGE_ACCESS_TOKEN'),
+  VALIDATION_TOKEN: (process.env.VALIDATION_TOKEN) ? (process.env.VALIDATION_TOKEN) : config.get('VALIDATION_TOKEN'),
+  APP_SECRET: (process.env.APP_SECRET) ? (process.env.APP_SECRET) : config.get('APP_SECRET'),
   api: 'https://techstar-bot-tortuvshin.c9users.io/api/data',
   app: app
 });
