3e05f96ed1a1d1c226f03eec718395fd58ec49f0
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2018-02-03 15:59:23 +0000

    update

--- techstar-bot/app.js
+++ techstar-bot/app.js
@@ -7,11 +7,27 @@ const express = require( 'express'),
 const app = express();
 
 
+const PAGE_ACCESS_TOKEN = (process.env.PAGE_ACCESS_TOKEN) ?
+  (process.env.PAGE_ACCESS_TOKEN) :
+  config.get('PAGE_ACCESS_TOKEN');
+  
+const VALIDATION_TOKEN = (process.VALIDATION_TOKEN.DB_URI) ?
+  (process.env.VALIDATION_TOKEN) :
+  config.get('VALIDATION_TOKEN');
+  
+const APP_SECRET = (process.env.APP_SECRET) ?
+  (process.env.APP_SECRET) :
+  config.get('APP_SECRET');
+  
+const api = (process.env.api) ?
+  (process.env.api) :
+  config.get('api');
+  
 const bot = new TechstarBot({
-  PAGE_ACCESS_TOKEN: (process.env.PAGE_ACCESS_TOKEN) ? (process.env.PAGE_ACCESS_TOKEN) : config.get('PAGE_ACCESS_TOKEN'),
-  VALIDATION_TOKEN: (process.env.VALIDATION_TOKEN) ? (process.env.VALIDATION_TOKEN) : config.get('VALIDATION_TOKEN'),
-  APP_SECRET: (process.env.APP_SECRET) ? (process.env.APP_SECRET) : config.get('APP_SECRET'),
-  api: (process.env.api) ? (process.env.api) : 'https://techstar-bot-tortuvshin.c9users.io/api/data',
+  PAGE_ACCESS_TOKEN: PAGE_ACCESS_TOKEN,
+  VALIDATION_TOKEN: VALIDATION_TOKEN,
+  APP_SECRET: APP_SECRET,
+  api: api,
   app: app
 });
 bot.initWebhook();
