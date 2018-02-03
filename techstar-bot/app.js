c8b5ba7f2dc564cd0b0f21d89c89f02af12462fd
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2018-02-03 15:38:35 +0000

    procfile update

--- techstar-bot/app.js
+++ techstar-bot/app.js
@@ -11,7 +11,7 @@ const bot = new TechstarBot({
   PAGE_ACCESS_TOKEN: (process.env.PAGE_ACCESS_TOKEN) ? (process.env.PAGE_ACCESS_TOKEN) : config.get('PAGE_ACCESS_TOKEN'),
   VALIDATION_TOKEN: (process.env.VALIDATION_TOKEN) ? (process.env.VALIDATION_TOKEN) : config.get('VALIDATION_TOKEN'),
   APP_SECRET: (process.env.APP_SECRET) ? (process.env.APP_SECRET) : config.get('APP_SECRET'),
-  api: 'https://techstar-bot-tortuvshin.c9users.io/api/data',
+  api: (process.env.api) ? (process.env.api) : 'https://techstar-bot-tortuvshin.c9users.io/api/data',
   app: app
 });
 bot.initWebhook();
