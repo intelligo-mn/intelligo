cb27d55db9b0d8af3dfffb1f8306e424dea880d7
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2018-02-03 16:25:32 +0000

    update

--- techstar-server/app.js
+++ techstar-server/app.js
@@ -7,7 +7,7 @@ var app = express();
 // Sub-Apps
 
 var api     = require("../techstar-api/app");
-var bot     = require("../techstar-bot/app");
+// var bot     = require("../techstar-bot/app");
 var console = require("../techstar-console/app");
 // var url     = require("../techstar-shortener/app");
 var errors  = require("../techstar-handler/app");
@@ -19,7 +19,7 @@ app.use("/api", api);
 // app.use("/url", url);
 app.use("/analytic", analytic);
 app.use("/uptime", uptime);
-app.use("/bot", bot);
+// app.use("/bot", bot);
 app.use(errors);
 
 module.exports = app;
