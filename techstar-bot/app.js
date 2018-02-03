12a9b757e68ffecaa6bb618124236cb0b12d7ef0
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2018-02-03 16:29:07 +0000

    app error fix

--- techstar-bot/app.js
+++ techstar-bot/app.js
@@ -6,12 +6,11 @@ const express = require( 'express'),
 
 const app = express();
 
-
 const PAGE_ACCESS_TOKEN = (process.env.PAGE_ACCESS_TOKEN) ?
   (process.env.PAGE_ACCESS_TOKEN) :
   config.get('PAGE_ACCESS_TOKEN');
   
-const VALIDATION_TOKEN = (process.VALIDATION_TOKEN.DB_URI) ?
+const VALIDATION_TOKEN = (process.VALIDATION_TOKEN) ?
   (process.env.VALIDATION_TOKEN) :
   config.get('VALIDATION_TOKEN');
   
