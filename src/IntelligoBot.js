416a56a4eeed9b60f9b9804bcff31ee1796019f0
Timo <30553356+y21@users.noreply.github.com>
2018-05-05 00:03:35 +0200

    const

--- src/IntelligoBot.js
+++ src/IntelligoBot.js
@@ -269,24 +269,24 @@ class IntelligoBot extends EventEmitter{
     }
   
   verifyRequestSignature(req, res, buf) {
-    var signature = req.headers["x-hub-signature"];
-  
-    if (!signature) {
-      console.error("Couldn't validate the signature.");
-    } else {
-      var elements = signature.split('=');
-      var method = elements[0];
-      var signatureHash = elements[1];
-  
-      var expectedHash = crypto.createHmac('sha1', this.APP_SECRET)
-                          .update(buf)
-                          .digest('hex');
-  
-      if (signatureHash != expectedHash) {
-        throw new Error("Couldn't validate the request signature.");
-      }
+        const signature = req.headers["x-hub-signature"];
+
+        if (!signature) {
+            console.error("Couldn't validate the signature.");
+        } else {
+            const elements = signature.split('='),
+                method = elements[0],
+                signatureHash = elements[1];
+
+            const expectedHash = crypto.createHmac('sha1', this.APP_SECRET)
+                .update(buf)
+                .digest('hex');
+
+            if (signatureHash != expectedHash) {
+                throw new Error("Couldn't validate the request signature.");
+            }
+        }
     }
-  }
   
   setGreeting(text){
     request({
