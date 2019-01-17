1f7bec57863e7d0e7f15d933ee3de051a64451e1
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2019-01-18 04:15:26 +0900

    updated

--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -435,22 +435,22 @@
         <div class="row align-items-center justify-content-xl-between">
           <div class="col-xl-6">
             <div class="copyright text-center text-xl-left text-muted">
-              &copy; 2018 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+              &copy; 2019 <a href="https://www.intelligo.systems" class="font-weight-bold ml-1" target="_blank">Intelligo Systems</a>
             </div>
           </div>
           <div class="col-xl-6">
             <ul class="nav nav-footer justify-content-center justify-content-xl-end">
               <li class="nav-item">
-                <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
+                <a href="https://www.intelligo.systems" class="nav-link" target="_blank">Intelligo Systems</a>
               </li>
               <li class="nav-item">
-                <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
+                <a href="https://www.intelligo.systems" class="nav-link" target="_blank">About Us</a>
               </li>
               <li class="nav-item">
-                <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
+                <a href="https://www.intelligo.systems" class="nav-link" target="_blank">Blog</a>
               </li>
               <li class="nav-item">
-                <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
+                <a href="https://www.intelligo.systems" class="nav-link" target="_blank">MIT License</a>
               </li>
             </ul>
           </div>
