ad229e070f0fea9c52efe95db5884a23c8c3588f
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2019-01-29 10:36:39 +0000

    updated

--- server/app.js
+++ server/app.js
@@ -6,14 +6,12 @@ var app = express();
 
 // Sub-Apps
 
-var bot     = require('../bot/app');
 var console = require('../console/app');
 var errors  = require('../handler/app');
 var uptime  = require('./uptime');
 
 app.use('/', console);
 app.use('/uptime', uptime);
-app.use('/bot', bot);
 app.use(errors);
 
 module.exports = app;
