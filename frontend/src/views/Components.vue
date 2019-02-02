e15eafb3ff9e0e26a16ed0ef01d8ddc8cd8e01e1
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2019-02-02 21:41:03 +0900

    update home

--- frontend/src/views/Components.vue
+++ frontend/src/views/Components.vue
@@ -1,18 +1,76 @@
 <template>
     <div>
         <hero></hero>
-        <download-section></download-section>
+        <section class="section section-lg pt-lg-100">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-12">
+                        <div class="row row-grid">
+                            <div class="col-lg-4">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <icon name="ni ni-check-bold" type="primary" rounded class="mb-4">
+                                    </icon>
+                                    <h6 class="text-primary text-uppercase">Download Argon</h6>
+                                    <p class="description mt-3">Argon is a great free UI package based on Bootstrap 4
+                                        that includes the most important components and features.</p>
+                                    <div>
+                                        <badge type="primary" rounded>design</badge>
+                                        <badge type="primary" rounded>system</badge>
+                                        <badge type="primary" rounded>creative</badge>
+                                    </div>
+                                    <base-button tag="a" href="#" type="primary" class="mt-4">
+                                        Learn more
+                                    </base-button>
+                                </card>
+                            </div>
+                            <div class="col-lg-4">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <icon name="ni ni-istanbul" type="success" rounded class="mb-4">
+                                    </icon>
+                                    <h6 class="text-success text-uppercase">Build Something</h6>
+                                    <p class="description mt-3">Argon is a great free UI package based on Bootstrap
+                                        4 that includes the most important components and features.</p>
+                                    <div>
+                                        <badge type="success" rounded>business</badge>
+                                        <badge type="success" rounded>vision</badge>
+                                        <badge type="success" rounded>success</badge>
+                                    </div>
+                                    <base-button tag="a" href="#" type="success" class="mt-4">
+                                        Learn more
+                                    </base-button>
+                                </card>
+                            </div>
+                            <div class="col-lg-4">
+                                <card class="border-0" hover shadow body-classes="py-5">
+                                    <icon name="ni ni-planet" type="warning" rounded class="mb-4">
+                                    </icon>
+                                    <h6 class="text-warning text-uppercase">Prepare Launch</h6>
+                                    <p class="description mt-3">Argon is a great free UI package based on Bootstrap
+                                        4 that includes the most important components and features.</p>
+                                    <div>
+                                        <badge type="warning" rounded>marketing</badge>
+                                        <badge type="warning" rounded>product</badge>
+                                        <badge type="warning" rounded>launch</badge>
+                                    </div>
+                                    <base-button tag="a" href="#" type="warning" class="mt-4">
+                                        Learn more
+                                    </base-button>
+                                </card>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 <script>
 import Hero from "./components/Hero";
-import DownloadSection from "./components/DownloadSection";
 
 export default {
   name: "components",
   components: {
-    Hero,
-    DownloadSection
+    Hero
   }
 };
 </script>
--- frontend/src/views/components/Hero.vue
+++ frontend/src/views/components/Hero.vue
@@ -1,16 +1,7 @@
 <template>
     <section class="section-hero section-shaped my-0">
         <div class="shape shape-style-1 shape-primary">
-            <span class="span-150"></span>
-            <span class="span-50"></span>
-            <span class="span-50"></span>
-            <span class="span-75"></span>
-            <span class="span-100"></span>
-            <span class="span-75"></span>
-            <span class="span-50"></span>
-            <span class="span-100"></span>
-            <span class="span-50"></span>
-            <span class="span-100"></span>
+
         </div>
         <div class="container shape-container d-flex align-items-center">
             <div class="col px-0">
@@ -21,7 +12,7 @@
 smart bots across mobile apps, web and messaging platforms.</p>
                         <div class="btn-wrapper">
                             <base-button tag="a"
-                                         href="/login"
+                                         href="login"
                                          class="mb-3 mb-sm-0"
                                          type="primary"
                                          icon="fa fa-user">
@@ -37,20 +28,7 @@ smart bots across mobile apps, web and messaging platforms.</p>
                         </div>
                     </div>
                 </div>
-                <div class="row align-items-center justify-content-around stars-and-coded">
-                    <div class="col-sm-4">
-                        <span class="text-white alpha-7 ml-3">Star us on</span>
-                        <a href="https://github.com/creativetimofficial/argon-design-system" target="_blank" title="Support us on Github">
-                            <img src="../../assets/images/brand/github-white-slim.png" style="height: 22px; margin-top: -3px">
-                        </a>
-                    </div>
-                    <div class="col-sm-4 mt-4 mt-sm-0 text-right">
-                        <span class="text-white alpha-7">Coded by</span>
-                        <a href="https://www.creative-tim.com" target="_blank" title="Creative Tim - Premium Bootstrap Themes and Templates">
-                            <img src="../../assets/images/brand/creativetim-white-slim.png" class="ml-3" style="height: 30px;">
-                        </a>
-                    </div>
-                </div>
+
             </div>
         </div>
     </section>
