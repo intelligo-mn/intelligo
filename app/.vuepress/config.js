c15fea3fe5675cacc1888d68b0143ae7b9555f72
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2019-03-11 19:06:48 +0900

    Added docs web

---
+++ app/.vuepress/config.js
@@ -0,0 +1,135 @@
+module.exports = {
+	title: 'Intelligo Framework',
+	dest: './docs',
+	themeConfig: {
+		repo: 'https://github.com/intelligo-systems/intelligo',
+		repoLabel: 'Github',
+		docsDir: './app',
+		editLinks: true,
+		logo: '/logo.png',
+		editLinkText: 'Found a bug? Help me improve this page!',
+		nav: [
+			{ text: 'Home', link: '/' }, 
+			{ text: 'Learn', link: '/learn/' }
+		],
+		version: '0.11.0-beta'
+	},
+	plugins: [
+		[
+			'@vuepress/google-analytics',
+			{
+				ga: 'UA-111622042-2'
+			}
+		],
+		[
+			'vuepress-plugin-rss',
+			{
+				base_url: '/',
+				site_url: 'https://intelligo.js.org',
+				filter: frontmatter => frontmatter.date <= new Date(),
+				count: 20
+			}
+		]
+	],
+	head: [
+		[
+			'link',
+			{ rel: 'apple-touch-icon', sizes: '57x57', href: '/apple-icon-57x57.png' }
+		],
+		[
+			'link',
+			{ rel: 'apple-touch-icon', sizes: '60x60', href: '/apple-icon-60x60.png' }
+		],
+		[
+			'link',
+			{ rel: 'apple-touch-icon', sizes: '72x72', href: '/apple-icon-72x72.png' }
+		],
+		[
+			'link',
+			{ rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-icon-76x76.png' }
+		],
+		[
+			'link',
+			{
+				rel: 'apple-touch-icon',
+				sizes: '114x114',
+				href: '/apple-icon-114x114.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'apple-touch-icon',
+				sizes: '120x120',
+				href: '/apple-icon-120x120.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'apple-touch-icon',
+				sizes: '144x144',
+				href: '/apple-icon-144x144.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'apple-touch-icon',
+				sizes: '152x152',
+				href: '/apple-icon-152x152.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'apple-touch-icon',
+				sizes: '180x180',
+				href: '/apple-icon-180x180.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '192x192',
+				href: '/android-icon-192x192.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '32x32',
+				href: '/favicon-32x32.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '96x96',
+				href: '/favicon-96x96.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '16x16',
+				href: '/favicon-16x16.png'
+			}
+		],
+		['link', { rel: 'manifest', href: '/manifest.json' }],
+		['meta', { name: 'msapplication-TileColor', content: '#ffffff' }],
+		[
+			'meta',
+			{ name: 'msapplication-TileImage', content: '/ms-icon-144x144.png' }
+		],
+		['meta', { name: 'theme-color', content: '#ffffff' }]
+	]
+}
---
+++ app/README.md
@@ -0,0 +1,10 @@
+---
+home: true
+heroImage: /logo.png
+heroText: Intelligo.js
+tagline: Intelligo is a JavaScript Framework to build Facebook Messenger's Chat bots.
+actionText: Get Started →
+actionLink: /learn/
+
+footer: MIT Licensed | Created by Intelligo Systems
+---
