1306964ed72f5f220edb8c93ef6c45fb9417be10
b.tortuvshin <b.tortuvshin@chinggis.systems>
2019-06-13 11:12:56 +0800

    Added chat thread

--- simulator/src/app/app.module.ts
+++ simulator/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatThreadComponent } from './components/chat-thread/chat-thread.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatThreadComponent
   ],
   imports: [
     BrowserModule,
---
+++ simulator/src/app/components/chat-thread/chat-thread.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-thread',
+  templateUrl: './chat-thread.component.html',
+  styleUrls: ['./chat-thread.component.scss']
+})
+export class ChatThreadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
