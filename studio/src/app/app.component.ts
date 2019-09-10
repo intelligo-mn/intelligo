1caf3c4e1df714df9745a53d35739dc59e0cb0bf
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2019-09-10 23:57:33 +0800

    updated app component

--- studio/src/app/app.component.ts
+++ studio/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { GlobalsService } from './services/globals.service';
+import { MatDialog } from '@angular/material';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'studio';
+	constructor(
+		public global: GlobalsService,
+		public router: Router,
+		public dialog: MatDialog) { }
+
+	loading() {
+		return this.global.loading;
+	}
+	hideLoading() {
+		this.global.loading = false;
+	}
+
+	goto(path: string) {
+		this.router.navigateByUrl(path);
+	}
 }
