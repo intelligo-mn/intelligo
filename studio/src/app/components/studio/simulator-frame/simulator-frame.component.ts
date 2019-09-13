32dc5e620d06dec59bd246c26c1a4c18bb20c209
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2019-09-13 11:13:48 +0800

    updated simulator frame title

--- studio/src/app/components/studio/simulator-frame/simulator-frame.component.ts
+++ studio/src/app/components/studio/simulator-frame/simulator-frame.component.ts
@@ -1,55 +1,55 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
-import { environment } from '../../../../environments/environment';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { SafeResourceUrl, DomSanitizer } from "@angular/platform-browser";
+import { environment } from "../../../../environments/environment";
 
 @Component({
-	selector: 'app-simulator-frame',
-	templateUrl: './simulator-frame.component.html',
-	styleUrls: ['./simulator-frame.component.scss']
+  selector: "app-simulator-frame",
+  templateUrl: "./simulator-frame.component.html",
+  styleUrls: ["./simulator-frame.component.scss"]
 })
 export class SimulatorFrameComponent implements OnInit {
+  constructor(private sanitizer: DomSanitizer) {
+    let param = {
+      brandingConfig: {
+        primaryBackgroundColor: "#5e72e4",
+        primaryForegroundColor: "white",
+        secondaryBackgroundColor: "#3c3c3c",
+        logoUrl: `favicon.ico`,
+        agentName: "Chatbots.mn",
+        frameHeight: "70vh",
+        frameWidth: "360px"
+      },
+      simulatorMode: true,
+      appConfig: {
+        htmlMessages: true
+      }
+    };
+    let url = `https://simulator.chatbots.mn/?s=${btoa(JSON.stringify(param))}`;
+    if (environment.local)
+      url = `http://localhost:4200/index.html?s=${btoa(JSON.stringify(param))}`;
+    this.iframeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 
-	constructor(private sanitizer: DomSanitizer) {
-		let param = {
-			brandingConfig: {
-				primaryBackgroundColor: '#5e72e4',
-				primaryForegroundColor: 'white',
-				secondaryBackgroundColor: '#3c3c3c',
-				logoUrl: `favicon.ico`,
-				agentName: "Chatbots.mn Simulator",
-				frameHeight: '70vh',
-				frameWidth: '360px',
-			},
-			simulatorMode: true,
-			appConfig: {
-				htmlMessages: true
-			}
-		};
-		let url = `https://simulator.chatbots.mn/?s=${btoa(JSON.stringify(param))}`;
-		if (environment.local)
-			url = `http://localhost:4200/index.html?s=${btoa(JSON.stringify(param))}`;
-		this.iframeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
-	}
+  ngOnInit() {}
+  iframeUrl: SafeResourceUrl;
+  isOpen: boolean = false;
 
-	ngOnInit() {
+  @ViewChild("anaRoot", { static: false })
+  anaRoot: ElementRef;
 
-	}
-	iframeUrl: SafeResourceUrl;
-	isOpen: boolean = false;
+  @ViewChild("simulatorIFrame", { static: false })
+  simulatorIFrame: ElementRef;
 
-	@ViewChild('anaRoot', {static: false})
-	anaRoot: ElementRef;
+  frame() {
+    return (this.simulatorIFrame.nativeElement as HTMLIFrameElement)
+      .contentWindow;
+  }
 
-	@ViewChild('simulatorIFrame', {static: false})
-	simulatorIFrame: ElementRef;
-
-	frame() {
-		return (this.simulatorIFrame.nativeElement as HTMLIFrameElement).contentWindow;
-	}
-
-	minMaxBtnClick() {
-		this.isOpen = !this.isOpen;
-		if (this.anaRoot && this.anaRoot.nativeElement)
-			(<HTMLDivElement>this.anaRoot.nativeElement).classList.remove('ana-hidden');
-	};
+  minMaxBtnClick() {
+    this.isOpen = !this.isOpen;
+    if (this.anaRoot && this.anaRoot.nativeElement)
+      (<HTMLDivElement>this.anaRoot.nativeElement).classList.remove(
+        "ana-hidden"
+      );
+  }
 }
