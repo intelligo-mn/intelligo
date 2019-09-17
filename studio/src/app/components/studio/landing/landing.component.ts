7d72ab6619cf21ae06c01d7c19b5d12aded90276
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2019-09-17 16:37:05 +0800

    updated node editor and landing page

--- studio/src/app/components/studio/landing/landing.component.ts
+++ studio/src/app/components/studio/landing/landing.component.ts
@@ -1,124 +1,142 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { Router } from '@angular/router';
-import { InfoDialogService } from '../../../services/info-dialog.service';
-import { GlobalsService } from '../../../services/globals.service';
-import { SettingsService } from '../../../services/settings.service';
-import * as models from '../../../models/chatflow.models';
-import { ObjectID } from 'bson';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { Router } from "@angular/router";
+import { InfoDialogService } from "../../../services/info-dialog.service";
+import { GlobalsService } from "../../../services/globals.service";
+import { SettingsService } from "../../../services/settings.service";
+import * as models from "../../../models/chatflow.models";
+import { ObjectID } from "bson";
 
 @Component({
-	selector: 'app-studio-landing',
-	templateUrl: './landing.component.html',
-	styleUrls: ['./landing.component.scss']
+  selector: "app-studio-landing",
+  templateUrl: "./landing.component.html",
+  styleUrls: ["./landing.component.scss"]
 })
 export class LandingComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private globals: GlobalsService,
+    private infoDialog: InfoDialogService,
+    private settings: SettingsService
+  ) {
+    this.globals.setPageTitle();
+    this.loadSavedProjects();
+  }
 
-	constructor(
-		private router: Router,
-		private globals: GlobalsService,
-		private infoDialog: InfoDialogService,
-		private settings: SettingsService) {
-		this.globals.setPageTitle();
-		this.loadSavedProjects();
-	}
+  @ViewChild("fileInput", { static: false })
+  fileInput: ElementRef;
 
-	@ViewChild('fileInput', { static: false })
-	fileInput: ElementRef;
+  loadSavedProjects() {
+    this.savedProjects = this.settings.listSavedChatProjectNames();
+  }
 
-	loadSavedProjects() {
-		this.savedProjects = this.settings.listSavedChatProjectNames();
-	}
+  savedProjects: string[] = [];
 
-	savedProjects: string[] = [];
+  ngOnInit() {}
 
-	ngOnInit() {
+  fileInputChange() {
+    let fileInput = this.fileInput.nativeElement as HTMLInputElement;
+    if (fileInput.files && fileInput.files[0]) {
+      let selectedFile = fileInput.files[0];
+      fileInput.value = "";
+      if (selectedFile.name.endsWith(".intelligo")) {
+        let reader: FileReader = new FileReader();
+        reader.onload = evt => {
+          let pack = JSON.parse(
+            reader.result.toString()
+          ) as models.ChatFlowPack;
+          let projName = selectedFile.name.replace(new RegExp(".intelligo$"), "");
+          this.settings.saveChatProject(projName, pack, false, () => {
+            this.openChatBotProject(projName);
+          });
+        };
+        reader.onerror = () => {
+          this.infoDialog.alert("Oops!", "Unable to load the file!");
+        };
+        reader.readAsText(selectedFile, "UTF-8");
+      } else
+        this.infoDialog.alert(
+          "Oops!",
+          "Invalid file. Please select a valid Chatbots.mn project file"
+        );
+    }
+  }
+  search: string;
+  searchedProjects() {
+    if (this.search && this.search.length > 0)
+      return this.savedProjects.filter(
+        x => x.toLowerCase().indexOf(this.search.toLowerCase()) != -1
+      );
+    return this.savedProjects;
+  }
+  addProject() {
+    this.infoDialog.prompt(
+      "Chatbot Project Name",
+      "Enter a name for your new chatbot project",
+      name => {
+        if (!name) return;
 
-	}
+        let firstNode = {
+          Name: "New Node",
+          Id: new ObjectID().toHexString(),
+          Buttons: [],
+          Sections: [],
+          NodeType: models.NodeType.Combination,
+          TimeoutInMs: 0
+        };
+        let _id = new ObjectID().toHexString();
+        let defaultFlow: models.ChatFlowPack = {
+          ChatNodes: [firstNode],
+          CreatedOn: new Date(),
+          UpdatedOn: new Date(),
+          NodeLocations: {},
+          ProjectId: _id,
+          _id: _id
+        };
+        defaultFlow.NodeLocations[firstNode.Id] = { X: 500, Y: 500 };
+        this.settings.saveChatProject(name, defaultFlow, false, () => {
+          this.openChatBotProject(name);
+        });
+      }
+    );
+  }
 
-	fileInputChange() {
-		let fileInput = this.fileInput.nativeElement as HTMLInputElement;
-		if (fileInput.files && fileInput.files[0]) {
-			let selectedFile = fileInput.files[0];
-			fileInput.value = '';
-			if (selectedFile.name.endsWith('.anaproj')) {
-				let reader: FileReader = new FileReader();
-				reader.onload = (evt) => {
-					let pack = JSON.parse(reader.result.toString()) as models.ChatFlowPack;
-					let projName = selectedFile.name.replace(new RegExp('\.anaproj$'), '');
-					this.settings.saveChatProject(projName, pack, false, () => {
-						this.openChatBotProject(projName);
-					});
-				};
-				reader.onerror = () => {
-					this.infoDialog.alert('Oops!', 'Unable to load the file!');
-				};
-				reader.readAsText(selectedFile, "UTF-8");
-			} else
-				this.infoDialog.alert('Oops!', 'Invalid file. Please select a valid Chatbots.mn project file');
-		}
-	}
-	search: string;
-	searchedProjects() {
-		if (this.search && this.search.length > 0)
-			return this.savedProjects.filter(x => x.toLowerCase().indexOf(this.search.toLowerCase()) != -1);
-		return this.savedProjects;
-	}
-	addProject() {
-		this.infoDialog.prompt('Chatbot Project Name', 'Enter a name for your new chatbot project', (name) => {
-			if (!name)
-				return;
+  isExpanded(proj: string) {
+    return this.savedProjects.indexOf(proj) == this.savedProjects.length - 1;
+  }
 
-			let firstNode = {
-				Name: 'New Node',
-				Id: new ObjectID().toHexString(),
-				Buttons: [],
-				Sections: [],
-				NodeType: models.NodeType.Combination,
-				TimeoutInMs: 0
-			};
-			let _id = new ObjectID().toHexString();
-			let defaultFlow: models.ChatFlowPack = {
-				ChatNodes: [firstNode],
-				CreatedOn: new Date(),
-				UpdatedOn: new Date(),
-				NodeLocations: {},
-				ProjectId: _id,
-				_id: _id
-			};
-			defaultFlow.NodeLocations[firstNode.Id] = { X: 500, Y: 500 };
-			this.settings.saveChatProject(name, defaultFlow, false, () => {
-				this.openChatBotProject(name);
-			});
-		});
-	}
+  openChatBotProject(name: string) {
+    this.router.navigateByUrl(
+      "/studio/designer?proj=" + encodeURIComponent(name)
+    );
+  }
+  renameChatBotProject(name: string) {
+    this.infoDialog.prompt(
+      "Rename",
+      "Enter a new name: ",
+      newName => {
+        if (newName && name != newName) {
+          this.settings.renameChatProject(name, newName);
+          this.loadSavedProjects();
+        }
+      },
+      name
+    );
+  }
+  deleteChatBotProject(name: string) {
+    this.infoDialog.confirm(
+      "Sure?",
+      `Are you sure you want to delete '${name}'`,
+      ok => {
+        if (ok) {
+          this.settings.deleteChatProject(name);
+          this.loadSavedProjects();
+        }
+      }
+    );
+  }
 
-	isExpanded(proj: string) {
-		return this.savedProjects.indexOf(proj) == this.savedProjects.length - 1;
-	}
-
-	openChatBotProject(name: string) {
-		this.router.navigateByUrl('/studio/designer?proj=' + encodeURIComponent(name));
-	}
-	renameChatBotProject(name: string) {
-		this.infoDialog.prompt("Rename", 'Enter a new name: ', (newName) => {
-			if (newName && name != newName) {
-				this.settings.renameChatProject(name, newName);
-				this.loadSavedProjects();
-			}
-		}, name);
-	}
-	deleteChatBotProject(name: string) {
-		this.infoDialog.confirm('Sure?', `Are you sure you want to delete '${name}'`, (ok) => {
-			if (ok) {
-				this.settings.deleteChatProject(name);
-				this.loadSavedProjects();
-			}
-		});
-	}
-
-	downloadChatBotProject(name: string) {
-		let pack = this.settings.getChatProject(name);
-		this.globals.downloadTextAsFile(name + ".anaproj", JSON.stringify(pack));
-	}
-
+  downloadChatBotProject(name: string) {
+    let pack = this.settings.getChatProject(name);
+    this.globals.downloadTextAsFile(name + ".intelligo", JSON.stringify(pack));
+  }
 }
--- studio/src/app/components/studio/nodeeditor/nodeeditor.component.ts
+++ studio/src/app/components/studio/nodeeditor/nodeeditor.component.ts
@@ -1,111 +1,135 @@
-import { Component, Inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { MatDialogRef, MatTab, MatTabGroup, MAT_DIALOG_DATA } from '@angular/material';
-import { Hotkey, HotkeysService } from 'angular2-hotkeys';
-import * as models from '../../../models/chatflow.models';
-import { ChatFlowService } from '../../../services/chatflow.service';
-import { GlobalsService } from '../../../services/globals.service';
-import { InfoDialogService } from '../../../services/info-dialog.service';
+import { Component, Inject, OnDestroy, OnInit, ViewChild } from "@angular/core";
+import {
+  MatDialogRef,
+  MatTab,
+  MatTabGroup,
+  MAT_DIALOG_DATA
+} from "@angular/material";
+import { Hotkey, HotkeysService } from "angular2-hotkeys";
+import * as models from "../../../models/chatflow.models";
+import { ChatFlowService } from "../../../services/chatflow.service";
+import { GlobalsService } from "../../../services/globals.service";
+import { InfoDialogService } from "../../../services/info-dialog.service";
 
 @Component({
-	selector: 'app-nodeeditor',
-	templateUrl: './nodeeditor.component.html',
-	styleUrls: ['./nodeeditor.component.scss']
+  selector: "app-nodeeditor",
+  templateUrl: "./nodeeditor.component.html",
+  styleUrls: ["./nodeeditor.component.scss"]
 })
 export class NodeEditorComponent implements OnInit, OnDestroy {
-	constructor(
-		private chatFlowService: ChatFlowService,
-		private hotkeys: HotkeysService,
-		private infoDialog: InfoDialogService,
-		public dialogRef: MatDialogRef<NodeEditorComponent>,
-		@Inject(MAT_DIALOG_DATA) public chatNode: models.ChatNode,
-		public globalsService: GlobalsService) {
-		this.MH = new models.ModelHelpers(globalsService, infoDialog);
-	}
+  constructor(
+    private chatFlowService: ChatFlowService,
+    private hotkeys: HotkeysService,
+    private infoDialog: InfoDialogService,
+    public dialogRef: MatDialogRef<NodeEditorComponent>,
+    @Inject(MAT_DIALOG_DATA) public chatNode: models.ChatNode,
+    public globalsService: GlobalsService
+  ) {
+    this.MH = new models.ModelHelpers(globalsService, infoDialog);
+  }
 
-	@ViewChild("nodeInfoTab", { static: false })
-	nodeInfoTab: MatTab;
+  @ViewChild("nodeInfoTab", { static: false })
+  nodeInfoTab: MatTab;
 
-	@ViewChild("contentTab", { static: false })
-	contentTab: MatTab;
-	contentTabIndex = 1;
+  @ViewChild("contentTab", { static: false })
+  contentTab: MatTab;
+  contentTabIndex = 1;
 
-	@ViewChild("buttonsTab", { static: false })
-	buttonsTab: MatTab;
+  @ViewChild("buttonsTab", { static: false })
+  buttonsTab: MatTab;
 
-	@ViewChild("tabGroup", { static: false })
-	tabGroup: MatTabGroup;
+  @ViewChild("tabGroup", { static: false })
+  tabGroup: MatTabGroup;
 
-	keymapOnNodeEditor: Hotkey[] = [
-		new Hotkey(["t", "alt+t"], (e, s) => {
-			this.addNewSectionText();
-			return false;
-		}, [], "Add a new text content (if applicable)"),
-		new Hotkey(["b", "alt+b"], (e, s) => {
-			this.addNewButton();
-			return false;
-		}, [], "Add a new button")
-	];
+  keymapOnNodeEditor: Hotkey[] = [
+    new Hotkey(
+      ["t", "alt+t"],
+      (e, s) => {
+        this.addNewSectionText();
+        return false;
+      },
+      [],
+      "Add a new text content (if applicable)"
+    ),
+    new Hotkey(
+      ["b", "alt+b"],
+      (e, s) => {
+        this.addNewButton();
+        return false;
+      },
+      [],
+      "Add a new button"
+    )
+  ];
 
-	bindNodeEditorShortcuts() {
-		this.unbindNodeEditorShortcuts();
-		this.keymapOnNodeEditor.forEach(x => this.hotkeys.add(x));
-	}
+  bindNodeEditorShortcuts() {
+    this.unbindNodeEditorShortcuts();
+    this.keymapOnNodeEditor.forEach(x => this.hotkeys.add(x));
+  }
 
-	unbindNodeEditorShortcuts() {
-		this.keymapOnNodeEditor.forEach(x => this.hotkeys.remove(x));
-	}
+  unbindNodeEditorShortcuts() {
+    this.keymapOnNodeEditor.forEach(x => this.hotkeys.remove(x));
+  }
 
-	ngOnDestroy(): void {
-		this.unbindNodeEditorShortcuts();
-	}
+  ngOnDestroy(): void {
+    this.unbindNodeEditorShortcuts();
+  }
 
-	ngOnInit(): void {
-		this.bindNodeEditorShortcuts();
-		if (this.chatNode && this.contentTabVisible() && this.chatNode.Sections.length > 0) {
-			this.tabGroup.selectedIndex = this.contentTabIndex;
-		}
-	}
+  ngOnInit(): void {
+    this.bindNodeEditorShortcuts();
+    if (
+      this.chatNode &&
+      this.contentTabVisible() &&
+      this.chatNode.Sections.length > 0
+    ) {
+      this.tabGroup.selectedIndex = this.contentTabIndex;
+    }
+  }
 
-	contentTabVisible() {
-		return ['Card', 'Combination'].indexOf(this.chatNode.NodeType) != -1;
-	}
+  contentTabVisible() {
+    return ["Card", "Combination"].indexOf(this.chatNode.NodeType) != -1;
+  }
 
-	addNewSectionText() {
-		if (this.contentTab) {
-			let newIndex = this.tabGroup._tabs.toArray().findIndex(x => x.textLabel == this.contentTab.textLabel);
-			if (this.tabGroup.selectedIndex == newIndex) {
-				this.MH.addSection(this.chatNode, models.SectionType.Text);
-			} else {
-				this.tabGroup.selectedIndex = newIndex;
-				setTimeout(() => {
-					requestAnimationFrame(() => {
-						this.MH.addSection(this.chatNode, models.SectionType.Text);
-					});
-				}, 500);
-			}
-		}
-	}
+  addNewSectionText() {
+    if (this.contentTab) {
+      let newIndex = this.tabGroup._tabs
+        .toArray()
+        .findIndex(x => x.textLabel == this.contentTab.textLabel);
+      if (this.tabGroup.selectedIndex == newIndex) {
+        this.MH.addSection(this.chatNode, models.SectionType.Text);
+      } else {
+        this.tabGroup.selectedIndex = newIndex;
+        setTimeout(() => {
+          requestAnimationFrame(() => {
+            this.MH.addSection(this.chatNode, models.SectionType.Text);
+          });
+        }, 500);
+      }
+    }
+  }
 
-	addNewButton() {
-		if (this.buttonsTab) {
-			let newIndex = this.tabGroup._tabs.toArray().findIndex(x => x.textLabel == this.buttonsTab.textLabel);
-			if (this.tabGroup.selectedIndex == newIndex) {
-				this.MH.addButton(this.chatNode);
-			} else {
-				this.tabGroup.selectedIndex = newIndex;
-				setTimeout(() => {
-					requestAnimationFrame(() => {
-						this.MH.addButton(this.chatNode);
-					});
-				}, 500);
-			}
-		}
-	}
+  addNewButton() {
+    if (this.buttonsTab) {
+      let newIndex = this.tabGroup._tabs
+        .toArray()
+        .findIndex(x => x.textLabel == this.buttonsTab.textLabel);
+      if (this.tabGroup.selectedIndex == newIndex) {
+        this.MH.addButton(this.chatNode);
+      } else {
+        this.tabGroup.selectedIndex = newIndex;
+        setTimeout(() => {
+          requestAnimationFrame(() => {
+            this.MH.addButton(this.chatNode);
+          });
+        }, 500);
+      }
+    }
+  }
 
-	SectionType = models.SectionType;
-	dismiss() {
-		this.dialogRef.close();
-	}
-	//Model Helpers
-	MH: models.ModelHelpers;
+  SectionType = models.SectionType;
+  dismiss() {
+    this.dialogRef.close();
+  }
+  //Model Helpers
+  MH: models.ModelHelpers;
 }
