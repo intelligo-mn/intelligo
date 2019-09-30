0075535f40cc8bc4fd0f6b0b287d8ab3a96ba778
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2019-09-30 23:53:47 +0800

    updated info dialog

--- studio/src/app/components/shared/info-dialog/info-dialog.component.ts
+++ studio/src/app/components/shared/info-dialog/info-dialog.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatSnackBar } from '@angular/material';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
 	selector: 'app-info-dialog',
