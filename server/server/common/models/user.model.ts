70c70d14ed72d7e432e05f7974564cd4a41e7e34
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2019-10-04 02:07:29 +0800

    fixed conflict

--- server/server/common/models/user.model.ts
+++ server/server/common/models/user.model.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
@@ -33,39 +32,3 @@ const UserSchema = new mongoose.Schema(
 );
 
 export const User = mongoose.model("User", UserSchema);
-=======
-import { Document, Schema, Model, model, Error } from "mongoose";
-import bcrypt from "bcrypt-nodejs";
-
-export interface IUser extends Document {
-  username: string;
-  password: string;
-}
-
-export const userSchema: Schema = new Schema({
-  username: String,
-  password: String,
-});
-
-
-userSchema.pre<IUser>("save", function save(next) {
-  const user = this;
-
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) { return next(err); }
-    bcrypt.hash(this.password, salt, undefined, (err: Error, hash) => {
-      if (err) { return next(err); }
-      user.password = hash;
-      next();
-    });
-  });
-});
-
-userSchema.methods.comparePassword = function (candidatePassword: string, callback: any) {
-  bcrypt.compare(candidatePassword, this.password, (err: Error, isMatch: boolean) => {
-    callback(err, isMatch);
-  });
-};
-
-export const User: Model<IUser> = model<IUser>("User", userSchema);
->>>>>>> 32bffb21f4ae884aff94bc3e90f356fff2d93ef7
