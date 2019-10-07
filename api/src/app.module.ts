70dc497289b50003999df0058bc77e03f57503fa
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2019-10-07 23:10:28 +0800

    added mongoose module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://turtuvshin:turtuvshin1@ds211648.mlab.com:11648/chatbotsmn')],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
