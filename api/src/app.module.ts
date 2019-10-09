14582e54f8209a21c51ad69553a3c1841eb3b2b4
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2019-10-09 22:08:18 +0800

    generated core module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { CoreModule } from './core/core.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://turtuvshin:turtuvshin1@ds211648.mlab.com:11648/chatbotsmn'), AuthModule, UsersModule],
+  imports: [MongooseModule.forRoot('mongodb://turtuvshin:turtuvshin1@ds211648.mlab.com:11648/chatbotsmn'), AuthModule, CoreModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
