dfc5602533c5e0f96f48493ca0cbed61d717ada6
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2019-10-16 20:40:34 +0800

    host config

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,17 +6,24 @@ import * as cors from 'cors';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { uptimeBot } from './integrations/uptime-bot';
-
+import * as vhost from 'vhost';
 if (process.env.NODE_ENV === 'test') {
   process.env.MONGO_URI = process.env.MONGO_URI_TEST;
   console.log('----------TESTING IN PROCESS----------');
 }
 
 const server = Express();
+const marketplace = Express();
+const studio = Express();
+const simulator = Express();
 server.use(cors());
 server.use(uptimeBot);
 server.get('/', (req, res) => res.send('ok'));
-
+server.use(
+  vhost('demo.chatbots.mn', (req, res) => {
+    res.end('hello vhost demo desu');
+  }),
+);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
