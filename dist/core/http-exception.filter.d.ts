dc043d044a59b069217e48a3fe294ad6cddfbcb0
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2019-10-25 16:53:44 +0800

    remove and ignore build dist

--- dist/core/http-exception.filter.d.ts
+++
@@ -1,4 +0,0 @@
-import { ArgumentsHost, ExceptionFilter, HttpException } from '@nestjs/common';
-export declare class HttpExceptionFilter implements ExceptionFilter {
-    catch(exception: HttpException, host: ArgumentsHost): void;
-}
