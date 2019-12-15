1980d4eff3d2c18ebdfe99bbbb2e4b70f0d01baa
Turtvshin Byambaa <toroo.byamba@gmail.com>
2019-12-16 00:46:56 +0800

    core: updated info dialog

--- simulator/src/app/components/info-dialog/info-dialog.component.ts
+++ simulator/src/app/components/info-dialog/info-dialog.component.ts
@@ -2,89 +2,88 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
-	selector: 'app-info-dialog',
-	templateUrl: './info-dialog.component.html',
-	styleUrls: ['./info-dialog.component.scss']
+  selector: 'app-info-dialog',
+  templateUrl: './info-dialog.component.html',
+  styleUrls: ['./info-dialog.component.scss'],
 })
 export class InfoDialogComponent implements OnInit {
+  constructor(
+    private dialogRef: MatDialogRef<InfoDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public options: InfoDialogOptions,
+  ) {
+    this.dialogRef.disableClose = true;
 
-	constructor(
-		private dialogRef: MatDialogRef<InfoDialogComponent>,
-		@Inject(MAT_DIALOG_DATA) public options: InfoDialogOptions) {
-		this.dialogRef.disableClose = true;
+    if (!options) {
+      options = {
+        dialogType: InfoDialogType.Alert,
+        title: 'Title',
+        message: 'Message',
+      };
+    }
 
-		if (!options) {
-			options = {
-				dialogType: InfoDialogType.Alert,
-				title: 'Title',
-				message: 'Message'
-			}
-		}
+    switch (options.dialogType) {
+      case InfoDialogType.Confirm:
+        {
+          this.primaryButtonText = this.options.primaryButton || 'OK';
+          this.secondaryButtonText = this.options.secondaryButton || 'Cancel';
+        }
+        break;
+      case InfoDialogType.Prompt:
+        {
+          this.inputText = options.defaultInputText;
+          this.primaryButtonText = this.options.primaryButton || 'Done';
+          this.secondaryButtonText = this.options.secondaryButton || 'Cancel';
+        }
+        break;
+      case InfoDialogType.Alert:
+      default:
+        {
+          this.secondaryButtonText = this.options.primaryButton || 'Close'; //Primary button plays as the text for secondaryButton here.
+        }
+        break;
+    }
+  }
 
-		switch (options.dialogType) {
-			case InfoDialogType.Confirm:
-				{
-					this.primaryButtonText = this.options.primaryButton || "OK";
-					this.secondaryButtonText = this.options.secondaryButton || "Cancel";
-				}
-				break;
-			case InfoDialogType.Prompt:
-				{
-					this.inputText = options.defaultInputText;
-					this.primaryButtonText = this.options.primaryButton || "Done";
-					this.secondaryButtonText = this.options.secondaryButton || "Cancel";
-				}
-				break;
-			case InfoDialogType.Alert:
-			default:
-				{
-					this.secondaryButtonText = this.options.primaryButton || "Close"; //Primary button plays as the text for secondaryButton here.
-				}
-				break;
-		}
-	}
+  ngOnInit() {}
 
-	ngOnInit() {
-	}
+  inputKeypress(event: KeyboardEvent) {
+    if (event.keyCode == 13) {
+      this.primaryClick();
+    }
+  }
 
-	inputKeypress(event: KeyboardEvent) {
-		if (event.keyCode == 13) {
-			this.primaryClick();
-		}
-	}
+  primaryClick() {
+    switch (this.options.dialogType) {
+      case InfoDialogType.Confirm:
+        this.dialogRef.close(true);
+        break;
+      case InfoDialogType.Prompt:
+        this.dialogRef.close(this.inputText);
+        break;
+      case InfoDialogType.Alert:
+      default:
+        this.dialogRef.close();
+        break;
+    }
+  }
+  inputText: string;
+  primaryButtonText: string;
+  secondaryButtonText: string;
 
-	primaryClick() {
-		switch (this.options.dialogType) {
-			case InfoDialogType.Confirm:
-				this.dialogRef.close(true);
-				break;
-			case InfoDialogType.Prompt:
-				this.dialogRef.close(this.inputText);
-				break;
-			case InfoDialogType.Alert:
-			default:
-				this.dialogRef.close();
-				break;
-		}
-	}
-	inputText: string;
-	primaryButtonText: string;
-	secondaryButtonText: string;
-
-	InfoDialogType = InfoDialogType;
+  InfoDialogType = InfoDialogType;
 }
 
 export interface InfoDialogOptions {
-	dialogType: InfoDialogType,
-	title: string,
-	message: string,
-	defaultInputText?: string,
-	primaryButton?: string;
-	secondaryButton?: string;
+  dialogType: InfoDialogType;
+  title: string;
+  message: string;
+  defaultInputText?: string;
+  primaryButton?: string;
+  secondaryButton?: string;
 }
 
 export enum InfoDialogType {
-	Prompt,
-	Alert,
-	Confirm
+  Prompt,
+  Alert,
+  Confirm,
 }
