eef4415eb5540460115309df92f7c678f5211aed
Turtvshin Byambaa <toroo.byamba@gmail.com>
2020-01-06 23:26:26 +0800

    fix: publish chatbot components

--- studio/src/app/components/shared/get-ana-chat-server/get-ana-chat-server.component.ts
+++ studio/src/app/components/shared/get-ana-chat-server/get-ana-chat-server.component.ts
@@ -2,26 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { AnaCloudSignupComponent } from '../ana-cloud-signup/ana-cloud-signup.component';
 @Component({
-	selector: 'app-get-ana-chat-server',
-	templateUrl: './get-ana-chat-server.component.html',
-	styleUrls: ['./get-ana-chat-server.component.scss']
+  selector: 'app-get-ana-chat-server',
+  templateUrl: './get-ana-chat-server.component.html',
+  styleUrls: ['./get-ana-chat-server.component.scss'],
 })
 export class GetAnaChatServerComponent implements OnInit {
+  constructor(private dialog: MatDialog) {}
 
-	constructor(
-		private dialog: MatDialog,
-	) { }
+  ngOnInit() {}
 
-	ngOnInit() {
-	}
+  selfHost() {
+    window.location.href = 'https://www.chatbots.mn/self-hosting';
+  }
 
-	selfHost() {
-		// this.electron.shell.openExternal('https://www.ana.chat/self-h・osting.html?r=' + Math.random());
-	}
-
-	anaCloud() {
-		let d = this.dialog.open(AnaCloudSignupComponent, {
-			width: 'auto'
-		});
-	}
+  anaCloud() {
+    let d = this.dialog.open(AnaCloudSignupComponent, {
+      width: 'auto',
+    });
+  }
 }
