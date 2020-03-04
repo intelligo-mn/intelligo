674051981577aaa9faac7c826ed0a872a7879012
Doljinsuren Enkhbayar <doljko927@gmail.com>
2020-03-05 00:56:01 +0800

    update simulator frame component

--- studio/src/app/components/studio/simulator-frame/simulator-frame.component.ts
+++ studio/src/app/components/studio/simulator-frame/simulator-frame.component.ts
@@ -1,28 +1,28 @@
-import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
-import { SafeResourceUrl, DomSanitizer } from "@angular/platform-browser";
-import { environment } from "../../../../environments/environment";
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
+import { environment } from '../../../../environments/environment';
 
 @Component({
-  selector: "app-simulator-frame",
-  templateUrl: "./simulator-frame.component.html",
-  styleUrls: ["./simulator-frame.component.scss"]
+  selector: 'app-simulator-frame',
+  templateUrl: './simulator-frame.component.html',
+  styleUrls: ['./simulator-frame.component.scss'],
 })
 export class SimulatorFrameComponent implements OnInit {
   constructor(private sanitizer: DomSanitizer) {
     let param = {
       brandingConfig: {
-        primaryBackgroundColor: "#5e72e4",
-        primaryForegroundColor: "white",
-        secondaryBackgroundColor: "#3c3c3c",
+        primaryBackgroundColor: '#5e72e4',
+        primaryForegroundColor: 'white',
+        secondaryBackgroundColor: '#3c3c3c',
         logoUrl: `favicon.ico`,
-        agentName: "Chatbots.mn",
-        frameHeight: "100%",
-        frameWidth: "360px"
+        agentName: 'Chatbots.mn',
+        frameHeight: '100%',
+        frameWidth: '360px',
       },
       simulatorMode: true,
       appConfig: {
-        htmlMessages: true
-      }
+        htmlMessages: true,
+      },
     };
     let url = `https://simulator.chatbots.mn/?s=${btoa(JSON.stringify(param))}`;
     if (environment.local)
@@ -34,10 +34,10 @@ export class SimulatorFrameComponent implements OnInit {
   iframeUrl: SafeResourceUrl;
   isOpen: boolean = false;
 
-  @ViewChild("intelligoRoot")
+  @ViewChild('intelligoRoot')
   intelligoRoot: ElementRef;
 
-  @ViewChild("simulatorIFrame")
+  @ViewChild('simulatorIFrame')
   simulatorIFrame: ElementRef;
 
   frame() {
@@ -49,7 +49,7 @@ export class SimulatorFrameComponent implements OnInit {
     this.isOpen = !this.isOpen;
     if (this.intelligoRoot && this.intelligoRoot.nativeElement)
       (<HTMLDivElement>this.intelligoRoot.nativeElement).classList.remove(
-        "intelligo-hidden"
+        'intelligo-hidden',
       );
   }
 }
