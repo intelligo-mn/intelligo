be1942d7050579d134a92328e2daf2fba3d6cb79
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-03-06 09:09:46 +0800

    updated chain delay service

--- simulator/src/app/services/chain-delay.service.ts
+++ simulator/src/app/services/chain-delay.service.ts
@@ -2,33 +2,33 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ChainDelayService {
-	constructor() { }
+  constructor() {}
 
-	private queue: any[] = [];
-	private timer: NodeJS.Timer;
+  private queue: any[] = [];
+  private timer: NodeJS.Timer;
 
-	private schedule(fn: (queueLength: number) => any, t: number) {
-		this.timer = setTimeout(() => {
-			this.timer = null;
-			fn(this.queue.length);
-			if (this.queue.length) {
-				let item = this.queue.shift();
-				this.schedule(item.fn, item.t);
-			}
-		}, t);
-	};
-	delay(fn: (queueLength: number) => any, t: number) {
-		// if already queuing things or running a timer, 
-		//   then just add to the queue
-		if (this.queue.length || this.timer) {
-			this.queue.push({ fn: fn, t: t });
-		} else {
-			// no queue or timer yet, so schedule the timer
-			this.schedule(fn, t);
-		}
-	};
-	cancel() {
-		clearTimeout(this.timer);
-		this.queue = [];
-	};
+  private schedule(fn: (queueLength: number) => any, t: number) {
+    this.timer = setTimeout(() => {
+      this.timer = null;
+      fn(this.queue.length);
+      if (this.queue.length) {
+        let item = this.queue.shift();
+        this.schedule(item.fn, item.t);
+      }
+    }, t);
+  }
+  delay(fn: (queueLength: number) => any, t: number) {
+    // if already queuing things or running a timer,
+    //   then just add to the queue
+    if (this.queue.length || this.timer) {
+      this.queue.push({ fn: fn, t: t });
+    } else {
+      // no queue or timer yet, so schedule the timer
+      this.schedule(fn, t);
+    }
+  }
+  cancel() {
+    clearTimeout(this.timer);
+    this.queue = [];
+  }
 }
