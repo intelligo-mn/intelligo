801c30786bb48f53ccd3a38e3496e1fdf377933a
Turtuvshin Byambaa <tortuvshin.b@chinggis.systems>
2020-03-12 09:31:30 +0800

    updated backend architect

---
+++ backend-market/src/integrations/intelligo-bot/index.ts
@@ -0,0 +1,19 @@
+import { MessengerBot } from 'intelligo';
+
+const bot = new MessengerBot({
+  PAGE_ACCESS_TOKEN: '',
+  VALIDATION_TOKEN: '',
+  APP_SECRET: '',
+});
+
+bot.initWebhook();
+
+//Subscribe to messages sent by the user with the bot.on() method.
+bot.on('message', event => {
+  const senderID = event.sender.id,
+    message = event.message;
+
+  if (message.text) {
+    bot.sendTextMessage(senderID, 'Hello world');
+  }
+});
